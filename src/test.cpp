c67d880300b7699072bca33fa836fd7e7966f2ab
Enrique Vicent <eccnil@gmail.com>
2020-09-01 02:04:47 +0200

    display

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,25 @@
-#include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include "display.hpp"
+
+Display display = Display();
+
+void setup(void)
+{
+  display.init();
+
+  display.print(1, "las doce tribus de mahadaonda");
+  display.print(2, "123456789012345");
+  display.print(3, "pepa");
+  display.print(3, "pip");
+  display.print(4, "pepa  4");
+  display.print(5, "pepa   5");
+  display.print(6, "pepa    6");
+  display.print(7, "pepa     7");
+  display.print(8, "pepa      8");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(void)
+{
+  delay(2000);
 }
