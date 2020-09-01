8b8790753192ae74283b3bbec7d3c2ea6783be75
Enrique Vicent <eccnil@gmail.com>
2020-09-01 16:31:29 +0200

    texto gráfico

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,11 @@
 
 #include <Arduino.h>
 #include "display.hpp"
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
 
 Display display = Display();
 
@@ -8,18 +13,19 @@ void setup(void)
 {
   display.init();
 
-  display.print(1, "las doce tribus de mahadaonda");
-  display.print(2, "123456789012345");
-  display.print(3, "pepa");
+  display.print(1, "las doce tribus de ", false);
+  display.print(2, "123456789012345", false);
+  display.print(3, "pepa", true);
   display.print(3, "pip");
   display.print(4, "pepa  4");
   display.print(5, "pepa   5");
   display.print(6, "pepa    6");
-  display.print(7, "pepa     7");
+  display.print(7, WiFi.macAddress().c_str());
   display.print(8, "pepa      8");
 }
 
+int i =0;
 void loop(void)
 {
-  delay(2000);
+  delay(200);
 }
