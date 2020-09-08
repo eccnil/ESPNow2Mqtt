fa19ec98fc21d02c91e03abe9f7de752a047e3ae
Enrique Vicent <eccnil@gmail.com>
2020-09-09 01:44:23 +0200

    test send

--- src/test.cpp
+++ src/test.cpp
@@ -50,7 +50,7 @@ void testChacha (){
   Serial.println(out);
 }
 
-void testProtobuff(){
+void testProtobuffResp(){
   int messageLength;
   response_OpResponse res1 =response_OpResponse_init_zero;
   res1.result_code = response_Result_NOMQTT;
@@ -79,13 +79,62 @@ void testProtobuff(){
 
 }
 
+void testProtobuffRq(){
+  int messageLength;
+
+  request_Ping ping1 = request_Ping_init_zero;
+  ping1.num=66;
+  request_Operation op1 = request_Operation_init_zero;
+  op1.op.ping = ping1;
+  op1.which_op = request_Operation_ping_tag;
+  request_Send send2 = request_Send_init_zero;
+  strcpy(send2.queue,"sendq");
+  strcpy(send2.payload,"{'mytest':0}");
+  send2.persist=true;
+  request_Operation op2 = request_Operation_init_zero;
+  op2.op.send = send2;
+  op2.which_op = request_Operation_send_tag;
+  request_Subscribe subs3 = request_Subscribe_init_zero;
+  strcpy(subs3.queue,"subsq");
+  subs3.clear=false;
+  request_Operation op3 = request_Operation_init_zero;
+  op3.op.qRequest = subs3;
+  op3.which_op = request_Operation_qRequest_tag;
+  request myRq = request_init_zero;
+  strcpy(myRq.client_id,"test");
+  myRq.operations[0]=op1;
+  myRq.operations[1]=op2;
+  myRq.operations[2]=op3;
+  myRq.operations_count=3;
+
+  uint8_t buffer[200];
+  pb_ostream_t myStream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+  auto encodeResult = pb_encode (&myStream, request_fields, &myRq);
+  messageLength = myStream.bytes_written;
+
+  Serial.println("request:");
+  Serial.println(encodeResult);
+  Serial.println(messageLength);
+  Serial.println((char*) buffer );
+
+  request decodedRq = request_init_default;
+  pb_istream_t iStream = pb_istream_from_buffer(buffer, messageLength);
+  pb_decode(&iStream, request_fields, &decodedRq);
+
+  Serial.println(decodedRq.client_id);
+  Serial.println(decodedRq.operations_count);
+
+
+}
+
 void setup() {
   display.init();
   Serial.begin(115200);
 
   displayMyMac();
   //testChacha();
-  testProtobuff();
+  //testProtobuffResp();
+  testProtobuffRq();
 }
 
 void loop() {
