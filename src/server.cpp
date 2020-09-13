d4ff90b413449c2567467651f524301ea17ff04b
Enrique Vicent <eccnil@gmail.com>
2020-09-13 13:24:17 +0200

    serial removed 2,6,5

--- src/server.cpp
+++ src/server.cpp
@@ -59,7 +59,7 @@ void onEspNowRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len
 }
 
 void setup() {
-    Serial.begin(115200);
+    //Serial.begin(115200);
     display.init();
     displayMyMac();
 
