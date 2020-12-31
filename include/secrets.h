fbbd4610583d81fae390259dcf2337e2682b8517
Enrique Vicent <eccnil@gmail.com>
2020-12-31 12:38:59 +0100

    secrets sample file

---
+++ include/secrets.h
@@ -0,0 +1,10 @@
+#ifndef _secrets_hpp_
+#define _secrets_hpp_
+
+const char* WIFI_SSID = "yourWifi";
+const char* WIFI_PASSWORD = "yourPassword123";
+const char* OTA_PASSWORD = "yourPassword000";
+
+const char* MQTT_SERVER_IP = "192.168.1.1";
+
+#endif
