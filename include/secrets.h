a662240a58a95c898638b27463c078861d851c90
Enrique Vicent <eccnil@gmail.com>
2021-01-04 19:48:30 +0100

    secret instructions

--- include/secrets.h
+++ include/secrets.h
@@ -8,3 +8,7 @@ const char* OTA_PASSWORD = "yourPassword000";
 const char* MQTT_SERVER_IP = "192.168.1.1";
 
 #endif
+/*
+ ignore changes with: git update-index --assume-unchanged include/secrets.h
+ undo with: git update-index --no-assume-unchanged include/secrets.h
+*/
